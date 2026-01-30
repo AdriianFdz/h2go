0e019f92d25a9c05b710b570d2d9185463b64f75
Adrian Fernandez <adriianfdzm@gmail.com>
2026-01-31 00:30:25 +0100

    fix: spacing and icons

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -5,7 +5,6 @@ import {
   TextField,
   Label,
   InputGroup,
-  Input,
   FieldError,
   InputGroupPrefix,
   InputGroupInput,
@@ -15,47 +14,46 @@ import { EnvelopeIcon, LockIcon } from "./components/icons";
 export default function Home() {
   return (
     <div>
-      <h1>H2GO</h1>
-      <h2>
-        The triple <span color="">&apos;T&apos;</span> solution
-      </h2>
-      <Form>
-        <TextField
-          isRequired
-          name="email"
-          type="email"
-          validate={(value) => {
-            if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(value)) {
-              return "Please enter a valid email address";
-            }
-            return null;
-          }}
-        >
-          <Label>Email</Label>
-          <InputGroup>
-            <InputGroupPrefix>
-              <EnvelopeIcon className="size-4 text-muted" />
-            </InputGroupPrefix>
-            <InputGroupInput placeholder="name@email.com" />
-          </InputGroup>
-          <FieldError />
-        </TextField>
+      <section className="max-w-50"></section>
+      <section className="max-w-50">
+        <Form className="space-y-3">
+          <TextField
+            isRequired
+            name="email"
+            type="email"
+            validate={(value) => {
+              if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(value)) {
+                return "Please enter a valid email address";
+              }
+              return null;
+            }}
+          >
+            <Label>Email</Label>
+            <InputGroup className="border border-muted focus-within:border-transparent">
+              <InputGroupPrefix>
+                <EnvelopeIcon className="size-4" />
+              </InputGroupPrefix>
+              <InputGroupInput placeholder="name@email.com" />
+            </InputGroup>
+            <FieldError />
+          </TextField>
 
-        <TextField
-          isRequired
-          name="password"
-          type="password"
-        >
-          <Label>Password</Label>
-          <InputGroup>
-            <InputGroupPrefix>
-              <LockIcon className="size-4 text-muted" />
-            </InputGroupPrefix>
-            <InputGroupInput placeholder="Enter your password" />
-          </InputGroup>
-          <FieldError />
-        </TextField>
-      </Form>
+          <TextField
+            isRequired
+            name="password"
+            type="password"
+          >
+            <Label>Password</Label>
+            <InputGroup className="border border-muted focus-within:border-transparent">
+              <InputGroupPrefix>
+                <LockIcon className="size-4" />
+              </InputGroupPrefix>
+              <InputGroupInput placeholder="Enter your password" />
+            </InputGroup>
+            <FieldError />
+          </TextField>
+        </Form>
+      </section>
     </div>
   );
 }

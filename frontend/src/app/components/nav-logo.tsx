b0bf63e6dd56035ae23259acd9be1a053cac027a
Adrian Fernandez <adriianfdzm@gmail.com>
2026-01-31 00:29:48 +0100

    feat: add logo component

---
+++ frontend/src/app/components/nav-logo.tsx
@@ -0,0 +1,18 @@
+import Image from "next/image";
+
+export const NavLogo = () => (
+  <div className="flex items-center space-x-3 p-4">
+    <Image
+      src="/logo.svg"
+      alt="H2GO Logo"
+      width={150}
+      height={150}
+    />
+    <div>
+      <h1 className="text-5xl font-black">H2GO</h1>
+      <p className="text-xs text-muted">
+        The triple <span className="font-bold">&apos;T&apos;</span> solution
+      </p>
+    </div>
+  </div>
+);

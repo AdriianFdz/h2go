6c515f08cec265daf2abed844da9b78aae223d22
Adrian Fernandez <adriianfdzm@gmail.com>
2026-02-03 19:12:03 +0100

    feat: implement getAllPendingRequests method to retrieve all pending requests

--- backend/src/requests/requests.controller.ts
+++ backend/src/requests/requests.controller.ts
@@ -2,7 +2,6 @@ import { Controller, Get, Req } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 import { RequestsService } from './requests.service';
 import { IAuthenticatedUser } from '../auth/interfaces/authenticatedUser';
-import { none } from '@hyperledger/fabric-gateway/dist/hash/hashes';
 
 @ApiTags('requests')
 @Controller('requests')
@@ -10,7 +9,7 @@ export class RequestsController {
   constructor(private requestsService: RequestsService) {}
   @Get()
   getAllPendingRequests(@Req() request) {
-    //TODO
-    return none;
+    const user: IAuthenticatedUser = request.user;
+    return this.requestsService.getAllPendingRequests(user);
   }
 }
